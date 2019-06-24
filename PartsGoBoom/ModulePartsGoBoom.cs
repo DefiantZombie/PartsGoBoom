85551ec38c5327a6738a4d6d0320eae93efda6ba
DefiantZombie <adobrinin@gmail.com>
2019-06-24 14:25:24 -0700

    Added FTS option to the PAM

--- PartsGoBoom/ModulePartsGoBoom.cs
+++ PartsGoBoom/ModulePartsGoBoom.cs
@@ -6,6 +6,15 @@
             advancedTweakable = true, requireFullControl = false)]
         public void ActivateFTS(KSPActionParam param)
         {
+            ActivateFTS();
+        }
+
+        [KSPEvent(active = true, advancedTweakable = true,
+            guiActive = true, guiActiveEditor = false,
+            guiActiveUncommand = true, guiActiveUnfocused = true,
+            requireFullControl = false, guiName = "#SSC_PartsGoBoom_000001")]
+        public void ActivateFTS()
+        {
             part.explode();
         }
     }
