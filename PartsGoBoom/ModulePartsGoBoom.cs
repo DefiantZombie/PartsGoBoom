dcad2132f966c43790e23435cdf75304cf75c9e4
DefiantZombie <adobrinin@gmail.com>
2019-06-24 17:01:20 -0700

    Implemented multi-mode FTS

--- PartsGoBoom/ModulePartsGoBoom.cs
+++ PartsGoBoom/ModulePartsGoBoom.cs
@@ -1,21 +1,92 @@
-﻿namespace PartsGoBoom
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PartsGoBoom
 {
     public class ModulePartsGoBoom : PartModule
     {
+        protected string[] ModeOptions = new string[]
+        {
+            "Single",
+            "Symmetry",
+            "Vessel"
+        };
+
         [KSPAction("#SSC_PartsGoBoom_000001",
             advancedTweakable = true, requireFullControl = false)]
         public void ActivateFTS(KSPActionParam param)
         {
-            ActivateFTS();
+            FTS();
         }
 
-        [KSPEvent(active = true, advancedTweakable = true,
-            guiActive = true, guiActiveEditor = false,
-            guiActiveUncommand = true, guiActiveUnfocused = true,
-            requireFullControl = false, guiName = "#SSC_PartsGoBoom_000001")]
-        public void ActivateFTS()
+        [KSPField(guiName = "#SSC_PartsGoBoom_000003", isPersistant = true,
+            guiActiveEditor = true, guiActive = true),
+            UI_ChooseOption(scene = UI_Scene.Editor)]
+        public string ftsMode = "Single";
+
+        [KSPAction("#SSC_PartsGoBoom_000002",
+            advancedTweakable = true, requireFullControl = false)]
+        public void ActivateNewFTS(KSPActionParam param)
+        {
+            ActivateNewFTS();
+        }
+
+        [KSPEvent(guiName = "#SSC_PartsGoBoom_000002", active = true, 
+            advancedTweakable = true, guiActive = true, 
+            guiActiveEditor = false, guiActiveUncommand = true, 
+            guiActiveUnfocused = true, requireFullControl = false)]
+        public void ActivateNewFTS()
+        {
+            if (ftsMode == "Single")
+            {
+                FTS();
+            }
+            else if(ftsMode == "Symmetry")
+            {
+                FTSMulti(part.symmetryCounterparts);
+                FTS();
+            }
+            else if(ftsMode == "Vessel")
+            {
+                FTSMulti(vessel.Parts, new Part[] { part });
+                FTS();
+            }
+        }
+
+        public void FTSMulti(List<Part> parts, Part[] exclude = null)
+        {
+            IEnumerable<Part> partsEx = exclude == null ? parts.ToArray() : parts.Except(exclude).ToArray();
+            foreach (Part p in partsEx)
+            {
+                ModulePartsGoBoom module = p.Modules.GetModule<ModulePartsGoBoom>();
+                if (module == null) continue;
+
+                module.FTS();
+            }
+        }
+
+        public void FTS()
         {
             part.explode();
+        }
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            
+            UpdateMenu();
+
+            PartsGoBoom.OnLanguageSwitchedEvent += UpdateMenu;
+        }
+
+        protected void UpdateMenu()
+        {
+            this.UpdateUIChooseOption(nameof(ftsMode), ModeOptions, PartsGoBoom.LocalStrings.Values.ToArray(), true, ftsMode);
+        }
+
+        private void OnDestroy()
+        {
+            PartsGoBoom.OnLanguageSwitchedEvent -= UpdateMenu;
         }
     }
 }
---
+++ PartsGoBoom/PartsGoBoom.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using KSP.Localization;
+
+namespace PartsGoBoom
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class PartsGoBoom : MonoBehaviour
+    {
+        public enum Strings
+        {
+            Single,
+            Symmetry,
+            Vessel
+        }
+
+        public static Dictionary<Strings, string> LocalStrings;
+
+        public static event Action OnLanguageSwitchedEvent;
+
+        private void Awake()
+        {
+            LocalStrings = new Dictionary<Strings, string>();
+
+            OnLanguageSwitched();
+        }
+
+        private void Start()
+        {
+            GameEvents.onLanguageSwitched.Add(OnLanguageSwitched);
+        }
+
+        private void OnLanguageSwitched()
+        {
+            LocalStrings[Strings.Single] = Localizer.Format("#SSC_PartsGoBoom_000004");
+            LocalStrings[Strings.Symmetry] = Localizer.Format("#SSC_PartsGoBoom_000005");
+            LocalStrings[Strings.Vessel] = Localizer.Format("#SSC_PartsGoBoom_000006");
+
+            OnLanguageSwitchedEvent?.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            GameEvents.onLanguageSwitched.Remove(OnLanguageSwitched);
+        }
+    }
+}
